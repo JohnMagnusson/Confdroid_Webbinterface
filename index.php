9f36dab0254d0f923023683a0dc860d24b077cfa
John Magnusson <johnvhmagnusson@gmail.com>
2017-04-04 11:31:01 +0200

    Mapped up css, javascript, pictures for structure

--- index.php
+++ index.php
@@ -2,31 +2,45 @@
 
 
 ?>
-<script type="text/javascript">
 
-    function helloWorld() {
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">        <!-- Adds css file -->
+</head>
+
+
+<body>
+
+<div id="container">
+
+    <h1>hello world</h1>
+
+    <form>
+
+        <input type="text" name="text" id="name"><br>
+        <input type="button" name="btn" onclick="helloWorld()" value="Hello world">
+
+    </form>
+
+</div>
+
+<script>
+
+    function helloWorld()
+    {
         var name = document.getElementById("name").value;
         alert("Hello " + name + " and HELLO WORLD!");
     }
 
 </script>
 
+</body>
 
-<header>
-
-    <body>
-
-        <h1>hello world</h1>
-
-        <form>
-
-            <input type="text" name="name" id="name"><br>
-
-            <input type="button" name="btn" onclick="helloWorld()" value="Hello world">
-
-        </form>
-
-    </body>
+</html>
 
 
-</header>
+
+
