29fd47154f90f3ab68dbacb3330017eba2ea61c2
Elias Axelsson <elle.axelsson@hotmail.com>
2017-05-02 10:23:57 +0200

    Added image

---
+++ javascript/DisplayedResult.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Elias on 2017-04-28.
+ */
+
+function DisplayedResult()
+{
+    this.template1 = new ResultTemplate();
+    this.template2 = new ResultTemplate();
+    this.template3 = new ResultTemplate();
+    this.template4 = new ResultTemplate();
+    this.template5 = new ResultTemplate();
+}
