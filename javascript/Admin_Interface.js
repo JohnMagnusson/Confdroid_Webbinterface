32baa082219c37e096be7ab4c42563ecad2bf95a
Elias Axelsson <elle.axelsson@hotmail.com>
2017-05-12 13:22:46 +0200

    Small fixes

--- javascript/Admin_Interface.js
+++ javascript/Admin_Interface.js
@@ -10,31 +10,6 @@ $(document).ready(function () {
         }
     }
 });
-function search()
-{
-    var searchType = $("#menu").find(".activeNav")[0].innerText;
-    var searchValue = document.getElementById("searchValue").value;
-    var url=window.location.href.split('/');
-    var name=url[url.length-1];
-    if(name.indexOf("Admin_Interface.php") < 0)
-    {
-        var url2 = "Admin_Interface.php?activeType="+$("#menu").find(".activeNav")[0].innerText+"&searchValue="+encodeURIComponent(searchValue)+"&searchType="+searchType;
-        window.location.href = url2;
-    }
-    else {
-        if(name.indexOf("searchValue") >= 0)
-        {
-            name = name.split("?")[1];
-            name = name.split("&");
-            searchValue = name[1].split("=")[1];
-            searchType = name[2].split("=")[1];
-            history.pushState({}, null, "Admin_Interface.php?activeType="+activeType);
-        }
-        getDataFromAPI(searchType, searchValue, function (users, searchType) {
-            printData(users, searchType)
-        });
-    }
-}
 
 function getDataFromAPI(searchType, searchValue, callback)
 {
